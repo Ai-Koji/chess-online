28b488180a6525e782832480fbd9c45e9e4e468f
Lost-S0ul <kireev.ivan.8@gmail.com>
2023-11-05 23:24:18 +0700

    feat: added main-forum page

---
+++ frontend/src/component/forum.js
@@ -0,0 +1,102 @@
+import React, { Component } from 'react';
+import "../styles/forum.css";
+import { Chessboard } from "react-chessboard";
+
+class Forum extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="header">
+                    <h1>cheeeeeess forum</h1>
+                    <input placeholder="Поиск" className="search-input" />
+                </div>
+                <table className="categories">
+                    <thead>
+                        <tr>
+                            <th className="subject"></th>
+                            <th>Темы</th>
+                            <th>сообщения</th>
+                            <th>последнее сообщение</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <a href="">
+                                    <h1>General Chess Discussion</h1>
+                                    <p>The place to discuss general chess topics</p>
+                                </a>
+                            </td>
+                            <td>65 097</td>
+                            <td>582 657</td>
+                            <td>
+                                <a href="">
+                                    5 минут назад
+                                    <div className="author">
+                                        WindyPawns
+                                    </div>
+                                </a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <a href="">
+                                    <h1>General Chess Discussion</h1>
+                                    <p>The place to discuss general chess topics</p>
+                                </a>
+                            </td>
+                            <td>65 097</td>
+                            <td>582 657</td>
+                            <td>
+                                <a href="">
+                                    5 минут назад
+                                    <div className="author">
+                                        WindyPawns
+                                    </div>
+                                </a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <a href="">
+                                    <h1>General Chess Discussion</h1>
+                                    <p>The place to discuss general chess topics</p>
+                                </a>
+                            </td>
+                            <td>65 097</td>
+                            <td>582 657</td>
+                            <td>
+                                <a href="">
+                                    5 минут назад
+                                    <div className="author">
+                                        WindyPawns
+                                    </div>
+                                </a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <a href="">
+                                    <h1>General Chess Discussion</h1>
+                                    <p>The place to discuss general chess topics</p>
+                                </a>
+                            </td>
+                            <td>65 097</td>
+                            <td>582 657</td>
+                            <td>
+                                <a href="">
+                                    5 минут назад
+                                    <div className="author">
+                                        WindyPawns
+                                    </div>
+                                </a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+}
+
+export default Forum
