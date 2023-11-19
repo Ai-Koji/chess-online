0efb04fb0fc0749cb030712076c271898650294e
Lost-S0ul <kireev.ivan.8@gmail.com>
2023-11-19 22:47:55 +0700

    feat: added message form

--- frontend/src/component/forum-page.js
+++ frontend/src/component/forum-page.js
@@ -3,49 +3,60 @@ import arrowBack from "../images/arrow-back.svg"
 import "../styles/forum-page.css";
 
 // to do:
-// add server request
-// add "add message" page
+// сделать обработку запросов
+// сделать кнопку "показать еще"
 
 class ForumPage extends React.Component {
-    render () {
+    render() {
         return (
             <div className="container">
                 <div className="header">
                     <button className="back">
-                        <img src={arrowBack}/>
+                        <img src={arrowBack} />
                     </button>
                     <h1>cheeeeeess forum</h1>
                 </div>
-                 
-                <div className="message">
-                    <div className="message-header">
-                        <div className="author">
-                            <h3>SimonBirch</h3>
+                <div className="message-list">
+
+                    <div className="message">
+                        <div className="message-header">
+                            <div className="author">
+                                <h3>SimonBirch</h3>
+                            </div>
+                            <div className="time">
+                                <h3>3 дня назад</h3>
+                            </div>
                         </div>
-                        <div className="time">
-                            <h3>3 дня назад</h3>
+                        <div className="message-main">
+                            <p>
+                                I’m messing with you of course, just getting a bit tired of these threads. How do I become etc xxx
+                            </p>
                         </div>
                     </div>
-                    <div className="message-main">
-                        <p>
-                        I’m messing with you of course, just getting a bit tired of these threads. How do I become etc xxx
-                        </p>
+                    <div className="message">
+                        <div className="message-header">
+                            <div className="user">
+                                <h3>SimonBirch</h3>
+                            </div>
+                            <div className="time">
+                                <h3>3 дня назад</h3>
+                            </div>
+                        </div>
+                        <div className="message-main">
+                            <p>
+                                I’m messing with you of course, just getting a bit tired of these threads. How do I become etc xxx
+                            </p>
+                        </div>
                     </div>
                 </div>
-                <div className="message">
-                    <div className="message-header">
-                        <div className="user">
-                            <h3>SimonBirch</h3>
-                        </div>
-                        <div className="time">
-                            <h3>3 дня назад</h3>
-                        </div>
-                    </div>
-                    <div className="message-main">
-                        <p>
-                        I’m messing with you of course, just getting a bit tired of these threads. How do I become etc xxx
-                        </p>
-                    </div>
+                <button>Показать еще</button>
+                <div className="answer-form">
+                    <form action="/submit" method="post">
+                        <h1>Ответить</h1>
+                        <textarea id="message" name="message" rows="4" placeholder="Введите ваше сообщение" required></textarea>
+
+                        <input type="submit" value="Отправить" />
+                    </form>
                 </div>
             </div>
         )
