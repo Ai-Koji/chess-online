c3d79c4b99d544f5461f4c82a0f51566ef92a49b
Ai-Koji <kireev.ivan.8@gmail.com>
2024-02-23 16:29:23 +0700

    feat: added new tables for mysql

--- backend/migrate.js
+++ backend/migrate.js
@@ -39,40 +39,71 @@ console.log('create tables');
 sqlCode = [
 	// User
 	`CREATE TABLE IF NOT EXISTS Users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    login VARCHAR(255) NOT NULL,
-    password TEXT NOT NULL,
-    email VARCHAR(255) NOT NULL
-  );`,
+		id INT AUTO_INCREMENT PRIMARY KEY,
+		login VARCHAR(255) NOT NULL,
+		password TEXT NOT NULL,
+		email VARCHAR(255) NOT NULL
+  	);`,
 	// Forum
 	`CREATE TABLE IF NOT EXISTS Forums (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    header TEXT NOT NULL,
-    about TEXT NOT NULL,
-    topic_count INT DEFAULT 0,
-    messages_count INT DEFAULT 0 
-  );`,
+		id INT AUTO_INCREMENT PRIMARY KEY,
+		header TEXT NOT NULL,
+		about TEXT NOT NULL,
+		topic_count INT DEFAULT 0,
+		messages_count INT DEFAULT 0 
+  	);`,
 	// Discussion
 	`CREATE TABLE IF NOT EXISTS Discussions (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    forum_class_id INT NOT NULL,
-    author INT NOT NULL,
-    header TEXT,
-    answer_count INT,
-    create_date DATETIME,
-    FOREIGN KEY (author) REFERENCES Users (id),
-    FOREIGN KEY (forum_class_id) REFERENCES Forums (id)
-  );`,
+		id INT AUTO_INCREMENT PRIMARY KEY,
+		forum_class_id INT NOT NULL,
+		author INT NOT NULL,
+		header TEXT,
+		answer_count INT,
+		create_date DATETIME,
+		FOREIGN KEY (author) REFERENCES Users (id),
+		FOREIGN KEY (forum_class_id) REFERENCES Forums (id)
+  	);`,
 	// Answer
 	`CREATE TABLE IF NOT EXISTS Answers (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL,
-    discussion_id INT NOT NULL,
-    content TEXT NOT NULL,
-    answer_date DATETIME,
-    FOREIGN KEY (user_id) REFERENCES Users (id),
-    FOREIGN KEY (discussion_id) REFERENCES Discussions (id)
-  );`
+		id INT AUTO_INCREMENT PRIMARY KEY,
+		user_id INT NOT NULL,
+		discussion_id INT NOT NULL,
+		content TEXT NOT NULL,
+		answer_date DATETIME,
+		FOREIGN KEY (user_id) REFERENCES Users (id),
+		FOREIGN KEY (discussion_id) REFERENCES Discussions (id)
+  	);`,
+	// image
+	`CREATE TABLE IF NOT EXISTS Image (
+		id INT AUTO_INCREMENT PRIMARY KEY,
+		name VARCHAR(255),
+		URL TEXT
+	);`,
+	// Pdf
+	`CREATE TABLE IF NOT EXISTS Pdf (
+		id INT AUTO_INCREMENT PRIMARY KEY,
+		name VARCHAR(255),
+		URL TEXT
+	);`,
+	// Book_class
+	`CREATE TABLE IF NOT EXISTS Book_class (
+		id INT AUTO_INCREMENT PRIMARY KEY,
+		header VARCHAR(50)
+	);`,
+	// Books
+	`CREATE TABLE IF NOT EXISTS Books (
+		id INT AUTO_INCREMENT PRIMARY KEY,
+		book_class_id INT NOT NULL,
+		title VARCHAR(50),
+		cost INT DEFAULT NULL,
+		author VARCHAR(50) DEFAULT "",
+		image INT,
+		aboutBook TEXT,
+		pdf INT,
+		FOREIGN KEY (book_class_id) REFERENCES Book_class (id),
+		FOREIGN KEY (image) REFERENCES Image (id),
+		FOREIGN KEY (pdf) REFERENCES  Pdf (id)
+	);`
 ];
 
 for (index = 0; index < sqlCode.length; index++) {
