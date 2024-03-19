99d1334d3e2d61bddb3b583840637ba94c35b6f9
Ai-Koji <kireev.ivan.8@gmail.com>
2024-03-19 10:05:41 +0700

    feat: added api for lessons

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -2,10 +2,12 @@ var express = require('express');
 var auth = require('./api/auth');
 var forum = require('./api/forum');
 var library = require('./api/library');
+var lesson = require('./api/lesson');
 var api = express.Router();
 
 api.use('/auth', auth);
 api.use('/forum', forum);
+api.use('/lessons', lesson);
 api.use('/library', library);
 
 module.exports = api;
---
+++ backend/routes/api/lesson.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var lesson = express.Router();
+var jwt = require('jsonwebtoken');
+const mysql = require('mysql');
+const { MYSQL, TOKENSECRET, DATABASENAME } = require('../../settings');
+const multer = require('multer');
+const upload = multer();
+
+// connect to mysql server
+const connection = mysql.createConnection({
+	host: MYSQL.host,
+	user: MYSQL.user,
+	password: MYSQL.password,
+	database: MYSQL.name
+});
+connection.connect();
+
+lesson.use((req, res, next) => {
+	res.append('Access-Control-Allow-Origin', ['*']);
+	res.append('Access-Control-Allow-Headers', 'Content-Type');
+	next();
+});
+
+lesson.get('/main', (req, res) => {
+	connection.query(
+		`
+        SELECT 
+            Lessons_blocks.id AS block_id,
+            Lessons_blocks.header AS block_header, 
+            Lessons.id AS lesson_id,
+            Lessons.header AS lesson_header,
+            Lessons.about AS lesson_about,
+            Lessons.image AS lesson_image,
+            Lessons.lesson_block AS lesson_block
+        FROM Lessons_blocks
+        INNER JOIN Lessons ON Lessons.lesson_block = Lessons_blocks.id
+        `,
+		(err, result) => {
+			if (err) res.sendStatus(500);
+			else {
+				res.send(result);
+			}
+		}
+	);
+});
+
+module.exports = lesson;
